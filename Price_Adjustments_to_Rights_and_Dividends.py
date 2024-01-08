5b3e866d23a8e034614748af37e67689389aec49
fei chen <fei3.chen2@outlook.com>
2024-01-08 18:25:31 +0800

    !@#$

--- Price_Adjustments_to_Rights_and_Dividends.py
+++ Price_Adjustments_to_Rights_and_Dividends.py
@@ -3,7 +3,9 @@ import traceback
 from datetime import datetime
 import numpy as np
 import pandas as pd
+pd.set_option("mode.chained_assignment", "warn")
 import akshare
+
 
 
 def price_adjustments_to_rights_and_dividends(tickers, path_prices, path_fhps, path_output):
@@ -56,7 +58,7 @@ def price_adjustments_to_rights_and_dividends(tickers, path_prices, path_fhps, p
         prices.to_csv(path_output + ticker + ".csv", encoding="gbk")
     return 0
 
-def check_missing_fhps_events(tickers, path_origin, path_hfq, path_fhps):
+def check_missing_fhps_events(tickers, path_origin, path_hfq, path_qfq, path_fhps):
     #利用AKShare东方财富原始收盘价及后复权收盘价，对比可得时间日期。
     if os.path.isfile(path_fhps + "check_log.csv"):
         check_log = pd.read_csv(path_fhps + "check_log.csv", dtype={"代码":str}, encoding="gbk")
@@ -65,37 +67,53 @@ def check_missing_fhps_events(tickers, path_origin, path_hfq, path_fhps):
     check_log.set_index(["代码"], inplace=True)
 
     for ticker in tickers:
-        if os.path.isfile(path_origin + ticker +".csv") and os.path.isfile(path_hfq + ticker +".csv") and os.path.isfile(path_fhps + ticker + ".csv"):
+        if os.path.isfile(path_origin + ticker +".csv") and os.path.isfile(path_hfq + ticker +".csv") and os.path.isfile(path_qfq + ticker +".csv"):
             price_origin = pd.read_csv(path_origin + ticker +".csv", encoding="gbk", index_col=0)
             price_hfq = pd.read_csv(path_hfq + ticker + ".csv", encoding="gbk", index_col=0)
-            fhps = pd.read_csv(path_fhps + ticker + ".csv", encoding="gbk", index_col=0)
+            price_qfq = pd.read_csv(path_qfq + ticker + ".csv", encoding="gbk", index_col=0)
+
             check = pd.DataFrame()
 
             check["原始收盘"] = price_origin["收盘"]
             check["后复权收盘"] = price_hfq["收盘"]
+            check["前复权收盘"] = price_qfq["收盘"]
+            check["日期"] = pd.to_datetime(price_origin["日期"])
+            check["日期"] = check["日期"].dt.strftime("%Y%m%d")
             check.index = pd.to_datetime(price_origin["日期"])
 
             check["原始收盘涨跌幅"] = check["原始收盘"].pct_change()
             check["后复权收盘涨跌幅"] = check["后复权收盘"].pct_change()
-            check.loc[check.index[0], "原始收盘涨跌幅"] = price_origin.loc[price_origin.index[0], "涨跌幅"]
-            check.loc[check.index[0], "后复权收盘涨跌幅"] = price_hfq.loc[price_hfq.index[0], "涨跌幅"]
+            check["前复权收盘涨跌幅"] = check["前复权收盘"].pct_change()
+            check.loc[check.index[0], "原始收盘涨跌幅"] = 0
+            check.loc[check.index[0], "后复权收盘涨跌幅"] = 0
+            check.loc[check.index[0], "前复权收盘涨跌幅"] = 0
 
-            # 两项涨跌幅相差0.003（0.3%）的，视为相同，即当日未发生股价调整。
-            check["调整发生日"] = (abs(check["原始收盘涨跌幅"]-check["后复权收盘涨跌幅"]) < 0.003)
-            events_checked = check[check["调整发生日"]==False]
-            fhps.set_index("除权除息日", inplace=True)
-            events_checked["日期"] = events_checked.index.strftime("%Y%m%d")
+            # 两项涨跌幅相差大于0.003（0.3%）的，视为相同，即当日发生股价调整。
+            check["后复权调整发生日"] = (abs(check["原始收盘涨跌幅"]-check["后复权收盘涨跌幅"]) > 0.003)
+            check["前复权调整发生日"] = (abs(check["原始收盘涨跌幅"]-check["前复权收盘涨跌幅"]) > 0.003)
+            events_checked = check[check["后复权调整发生日"] == True]
+            events_checked = events_checked[events_checked["前复权调整发生日"] == True]
 
             # 除权除息记录中未出现的日期
-            events_missing = events_checked[~events_checked.index.isin(fhps.index)]
+            if os.path.isfile(path_fhps + ticker + ".csv"):
+                fhps = pd.read_csv(path_fhps + ticker + ".csv", encoding="gbk", index_col=0)
+                fhps.set_index("除权除息日", inplace=True)
+                events_missing = events_checked[~events_checked.index.isin(fhps.index)]
+            else:
+                events_missing = events_checked
             if len(events_checked["日期"].tolist()) > 0:
                 check_log.loc[ticker, "所有调整日"] = "|".join(events_checked["日期"].tolist())
+            else:
+                check_log.loc[ticker, "所有调整日"] = ""
             if len(events_missing["日期"].tolist()) > 0:
                 check_log.loc[ticker, "未记录调整日"] = "|".join(events_missing["日期"].tolist())
-                check_log.loc[ticker, "最后未记录调整日"] = events_missing["日期"].tolist()[-1]
+                check_log.loc[ticker, "最后未记录调整日"]= events_missing["日期"].tolist()[-1]
+            else:
+                check_log.loc[ticker, "未记录调整日"] = ""
+                check_log.loc[ticker, "最后未记录调整日"]= ""
         else:
             pass
-
+        print("%s data checked" % ticker)
     check_log.to_csv(path_fhps + "check_log.csv", encoding="gbk")
     return 0
 
@@ -112,12 +130,12 @@ def akshare_fhps_info_update(ticker, path):
         # get fhps records from AKShare
         fhps_new = akshare.stock_fhps_detail_em(symbol=ticker)
     except:
-        log_message = [str(i) + " : failed to get data from AKShare website"]
+        log_message = [ticker + " : failed to get data from AKShare website"]
         log.loc[len(log)] = log_message
 
     else:
         if fhps_new.empty:
-            log_message = [str(i) + " : data from AKShare website is empty."]
+            log_message = [ticker + " : data from AKShare website is empty."]
             log.loc[len(log)] = log_message
         else:
             if os.path.isfile(path + ticker + ".csv"):
@@ -150,9 +168,11 @@ def akshare_fhps_info_update(ticker, path):
     print("Ticker %s updated successfully" % ticker)
     return 0
 
+
+
 if __name__ == '__main__':
     path = str.replace(os.getcwd(), "\\", "/")
     #akshare_fhps_info_update(["600276"], path + "/fhps/")
 
-    #check_missing_fhps_events(["600276"], path + "/stocks/origin/", path + "/stocks/hfq/", path + "/fhps/")
-    price_adjustments_to_rights_and_dividends(["600276"], path + "/stocks/origin/", path + "/fhps/", path + "/stocks/output/")
+    check_missing_fhps_events(["600276"], path + "/stocks/origin/", path + "/stocks/hfq/", path + "/stocks/qfq/", path + "/fhps/")
+    #price_adjustments_to_rights_and_dividends(["600276"], path + "/stocks/origin/", path + "/fhps/", path + "/stocks/output/")
